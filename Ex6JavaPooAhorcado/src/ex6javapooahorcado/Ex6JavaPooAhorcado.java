a5ef303fa302de172014be5d726d001c197a4492
sanrvallejo <zanti-23@hotmail.com>
2024-08-18 16:30:54 -0500

    foundLetter and game methods

--- Ex6JavaPooAhorcado/src/ex6javapooahorcado/Ex6JavaPooAhorcado.java
+++ Ex6JavaPooAhorcado/src/ex6javapooahorcado/Ex6JavaPooAhorcado.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ex6javapooahorcado;
 
-/**
- *
- * @author Usuario
- */
 public class Ex6JavaPooAhorcado {
 
     /**
--- Ex6JavaPooAhorcado/src/ex6javapooahorcado/Service.java
+++ Ex6JavaPooAhorcado/src/ex6javapooahorcado/Service.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Service {
     Scanner sc = new Scanner(System.in); 
     Ahorcado game = new Ahorcado();
+    String[] wordCopy = new String[game.getWord().length];
     String word;
     int attempts;
+    boolean foundWord = false;
     
     //methods
     
     public void createGame() {
+        //wordCopy
+        for (int i = 0; i < wordCopy.length; i++) {
+            wordCopy[i] = "_";
+        }
+        
         //set word vector
         System.out.println("Enter a word to guess");
         word = sc.nextLine();
@@ -22,18 +29,82 @@ public class Service {
         game.setAttempts(attempts);
     }
     
-    
     public void showLength() {
         System.out.println("Length of the word to guess" + game.getWord().length);
     }
     
-    public void searchLetter(String l){
+    public boolean foundLetter(String l){
+        boolean foundLetter = false;
+        foundLetter = searchLetter(l, foundLetter);
+        int i = 0;
+        
+        //modify attempts
+        if (foundLetter) {
+            for (String letter : game.getWord()) {
+                if (letter.equalsIgnoreCase(l)){
+                    wordCopy[i] = letter;
+                }else {
+                    attempts--;
+                    game.setAttempts(attempts);
+                } 
+                i++;
+            }
+        }
+        
+        return foundLetter;
+        
+    }
+    
+    public boolean searchLetter(String l, boolean foundLetter){
+        //compare letter with word array
         for (String letter : game.getWord()) {
             if (l.equalsIgnoreCase(letter)) {
-                //foundedLetter(l);
+                foundLetter = true;
             }else {
                 continue;
             }
         }
+        
+        return foundLetter;
+    }
+    
+    public void game() {
+        String letter;
+        String guessedWord = "";
+        
+        //game settings:
+        System.out.println("Game settings: Don't let the other player see these settings");
+        createGame();
+        
+        //game
+        System.out.printf("The word to guess has %d letters and ypu have %d attempts %n", 
+                game.getWord().length, game.getAttempts());
+        
+        while (game.getAttempts() > 0) {            
+            
+            System.out.println("Please, enter a letter");
+            letter = sc.nextLine();
+            foundLetter(letter);
+            if(foundLetter(letter)) {
+                System.out.println("Good guess");
+            }else {
+                System.out.println("Wrong guess");
+            }
+            
+            for (String l : wordCopy) {
+                guessedWord += (l + " ");
+            }
+            
+            System.out.printf("Your word: %d%n Attempts: %d%n", guessedWord, attempts);
+        }
+        
+        if(wordCopy.equals(game.getWord())) {
+            System.out.println("You won, game over");
+        }else {
+            System.out.println("You have lost, game over");
+        }
+            
+               
+        
     }
 }
